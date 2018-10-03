824a65eb9d7e1ba20eb55e85cf845ca908176804
zubbyonfire <zackbloundele@outlook.com>
2018-10-03 18:29:54 +0100

    Platform Implementation
    Begun implementing the platform logic - have created the basic variables to use and Gizmos to help with setting them up

---
+++ CubePlatformer/Assets/Scripts/TestMechanics/PlatformLogic.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLogic : MonoBehaviour {
+
+    //Platform movement variables
+    [SerializeField]
+    private float platformMoveSpeed;
+
+    [SerializeField]
+    [Range(0,5)]
+    private float startPos = 1, endPos = 1;
+
+    private Vector2 startTargetPos, endTargetPos;
+
+    //Platform logic
+    [Tooltip("Does this platform require player to be on to start moving")]
+    [SerializeField]
+    private bool playerTriggerActive = false;
+
+    [Tooltip("Does the platform continuosly move back and forth")]
+    [SerializeField]
+    private bool continuosMovement = false;
+
+    private enum MovementDirection
+    {
+        Horizontal,
+        Vertical
+    }
+
+    [SerializeField]
+    private MovementDirection movementDirection;
+
+    //Where does the platform start when the level begins
+    private enum PlatformStartPosition
+    {
+        None,
+        Start,
+        End
+    }
+
+    [SerializeField]
+    private PlatformStartPosition platformStartPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        switch (movementDirection)
+        {
+            case MovementDirection.Horizontal:
+                startTargetPos = new Vector2(transform.position.x + startPos, transform.position.y);
+                endTargetPos = new Vector2(transform.position.x - endPos, transform.position.y);
+                break;
+            case MovementDirection.Vertical:
+                startTargetPos = new Vector2(transform.position.x, transform.position.y + startPos);
+                endTargetPos = new Vector2(transform.position.x, transform.position.y - endPos);
+                break;
+        }
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, startTargetPos);
+        Gizmos.DrawCube(startTargetPos, new Vector3(0.25f, 0.25f, 0));
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, endTargetPos);
+        Gizmos.DrawCube(endTargetPos, new Vector3(0.25f, 0.25f, 0));
+    }
+
+    //Child the Player while they collide with the trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    //De-Child the player once they exit the trigger
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
