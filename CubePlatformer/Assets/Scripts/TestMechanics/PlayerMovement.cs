411268aecb737d09ef9425721dfc501bac9595c1
zubbyonfire <zackbloundele@outlook.com>
2018-10-11 22:00:42 +0100

    Wall Jump Update
    Removing wall jump for now, mechanic doesn't really work atm
    - Consider for future implementation

--- CubePlatformer/Assets/Scripts/TestMechanics/PlayerMovement.cs
+++ CubePlatformer/Assets/Scripts/TestMechanics/PlayerMovement.cs
@@ -31,12 +31,13 @@ public class PlayerMovement : MonoBehaviour, IPlatform
     private Vector3 velocity = Vector3.zero;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb2D = GetComponent<Rigidbody2D>();
 
         playerCollider = GetComponent<Collider2D>();
         distToSides = playerCollider.bounds.extents.x;
-        distToGround = playerCollider.bounds.extents.y;   
+        distToGround = playerCollider.bounds.extents.y;
     }
 
     public void Move(float move, bool jump, float jumpForce)
@@ -50,22 +51,12 @@ public class PlayerMovement : MonoBehaviour, IPlatform
 
         rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, targetVelocity, ref velocity, movementSmoothing);
 
+
         if (IsGrounded() && jump)
         {
             rb2D.AddForce(new Vector2(0, jumpForce));
         }
-        else if (!IsGrounded() && IsWallJumpLeft() && jump)
-        {
-            //Jump right
-            rb2D.AddForce(new Vector2(jumpForceLeftRight, jumpForce));
-            Debug.Log("Jump Right");
-        }
-        else if (!IsGrounded() && IsWallJumpRight() && jump)
-        {
-            //Jump left
-            rb2D.AddForce(new Vector2(-jumpForceLeftRight, jumpForce));
-            Debug.Log("Jump Left");
-        }
+
     }
 
     //Check if the Player is grounded
@@ -89,75 +80,18 @@ public class PlayerMovement : MonoBehaviour, IPlatform
         if (hit.collider != null || hit2.collider != null || hit3.collider != null)
         {
             return true;
-        } 
-
-        return false;
-    }
-
-    bool IsWallJumpLeft()
-    {
-        Vector2 position = transform.position;
-        Vector2 position1 = new Vector2(transform.position.x, transform.position.y + distToGround);
-        Vector2 position2 = new Vector2(transform.position.x, transform.position.y - distToGround);
-
-        Vector2 direction = Vector2.left;
-        float distance = distToSides + 0.1f;
-
-        Debug.DrawRay(position, direction * distance, Color.cyan);
-        Debug.DrawRay(position1, direction * distance, Color.magenta);
-        Debug.DrawRay(position2, direction * distance, Color.grey);
-
-        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance);
-        RaycastHit2D hit2 = Physics2D.Raycast(position1, direction, distance);
-        RaycastHit2D hit3 = Physics2D.Raycast(position2, direction, distance);
-
-        if (hit.collider != null && hit2.collider != null && hit3.collider != null)
-        {
-            if (hit.collider.tag == "WallJump" || hit2.collider.tag == "WallJump" || hit3.collider.tag == "WallJump")
-            {
-                return true;
-            }
         }
 
         return false;
     }
-
-    bool IsWallJumpRight()
-    {
-        Vector2 position = transform.position;
-        Vector2 position1 = new Vector2(transform.position.x, transform.position.y + distToGround);
-        Vector2 position2 = new Vector2(transform.position.x, transform.position.y - distToGround);
-
-        Vector2 direction = Vector2.right;
-        float distance = distToSides + 0.1f;
-
-        Debug.DrawRay(position, direction * distance, Color.cyan);
-        Debug.DrawRay(position1, direction * distance, Color.magenta);
-        Debug.DrawRay(position2, direction * distance, Color.grey);
-
-        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance);
-        RaycastHit2D hit2 = Physics2D.Raycast(position1, direction, distance);
-        RaycastHit2D hit3 = Physics2D.Raycast(position2, direction, distance); ;
-
-        if (hit.collider != null && hit2.collider != null && hit3.collider != null)
-        {
-            if (hit.collider.tag == "WallJump" || hit2.collider.tag == "WallJump" || hit3.collider.tag == "WallJump")
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
 
     public void OnPlatform(bool onPlatform)
-    { 
+    {
         if (onPlatform) //If on platform freeze rotation, otherwise unFreeze it
         {
             rb2D.constraints = RigidbodyConstraints2D.FreezeRotation;
 
-            transform.rotation = Quaternion.Euler(0,0,0); 
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         else
         {
